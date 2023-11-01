cf743be7f7d512527c6beeab5be85e5b875fe0b7
3sparrow <3sparrow@gmail.com>
2023-11-01 22:15:56 +0100

    pętle

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+var sex = "men" ;
+int age = 66;
+
+if
+    (sex == "women" && age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+else if 
+    (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+else if (sex == "men" && age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+else if (sex == "women" || sex == "men" && age > 65)
+    {
+        Console.WriteLine("Człowiek na emeryturze");
+    }

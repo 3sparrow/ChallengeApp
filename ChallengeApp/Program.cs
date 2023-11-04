285d0ee940e7ff9205d7578feb2575d4861da7d9
3sparrow <3sparrow@gmail.com>
2023-11-04 09:51:35 +0100

    zadanie dzień 5

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,22 +1,71 @@
-﻿string name = "Ewa";
-var sex = "men" ;
-int age = 33;
+﻿int number = 642426;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToCharArray();
+int zero = 0;
+int one = 0;
+int two = 0;
+int three = 0;
+int four = 0;
+int five = 0;
+int six = 0;
+int seven = 0;
+int eight = 0;
+int nine = 0;
 
-if
-    (sex == "women" && age < 30)
+
+foreach (char z in letters)
+{
+    if (z == '0')
     {
-        Console.WriteLine("Kobieta poniżej 30 lat");
+        zero++;
     }
-else if 
-    (name == "Ewa" && age == 33)
+    else if (z == '1')
     {
-        Console.WriteLine("Ewa, lat 33");
+        one++;
     }
-else if (sex == "men" && age < 18)
+    else if (z == '2')
     {
-        Console.WriteLine("Niepełnoletni mężczyzna");
+        two++;
     }
-else if (age > 65)
+    else if (z == '3')
     {
-        Console.WriteLine("Człowiek na emeryturze");
+        three++;
     }
+    else if (z == '4')
+    {
+        four++;
+    }
+    else if (z == '5')
+    {
+        five++;
+    }
+    else if (z == '6')
+    {
+        six++;
+    }
+    else if (z == '7')
+    {
+        seven++;
+    }
+    else if (z == '8')
+    {
+        eight++;
+    }
+    else if (z == '9')
+    {
+        nine++;
+    }
+
+
+
+}
+Console.WriteLine("Cyfr 0 jest " + zero);
+Console.WriteLine("Cyfr 1 jest " + one);
+Console.WriteLine("Cyfr 2 jest " + two);
+Console.WriteLine("Cyfr 3 jest " + three);
+Console.WriteLine("Cyfr 4 jest " + four);
+Console.WriteLine("Cyfr 5 jest " + five);
+Console.WriteLine("Cyfr 6 jest " + six);
+Console.WriteLine("Cyfr 7 jest " + seven);
+Console.WriteLine("Cyfr 8 jest " + eight);
+Console.WriteLine("Cyfr 9 jest " + nine);
